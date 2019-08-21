27e9f99102248e5a55dc56dd08f3794a5fee285e
Ji Kim <jkim0107@gmail.com>
2019-08-21 14:28:43 -0700

    connected server and serving html page

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//middleware
+const morgan = require('morgan');
+const parser = require('body-parser');
+
+//use middleware
+app.use(morgan('dev'));
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => {console.log(`listening on port ${port}`)});
