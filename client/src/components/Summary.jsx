70d5ad829ede69e7b544228f8112f60c563dfd35
Ji Kim <jkim0107@gmail.com>
2019-08-25 10:20:40 -0700

    added responsive placeholders for summary component

--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 const Summary = (props) => {
   return (
     <div className="flex-container">
-      <div className="summary-container">1</div>
-      <div className="summary-container">1</div>
-      <div className="summary-container">1</div>
+      <div className="summary-container">Average Stars & Review Count Here</div>
+      <div className="summary-container">% Recommended</div>
+      <div className="summary-container">Customers Say Average</div>
     </div>
   )
 }
