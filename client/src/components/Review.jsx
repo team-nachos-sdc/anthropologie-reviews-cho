d8461ca87f924e78b66e54e818f137cb5b657370
Ji Kim <jkim0107@gmail.com>
2019-09-02 12:45:49 -0700

    added review stars for each review

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import StarSummary from './StarSummary.jsx';
+import StarRating from './StarRating.jsx';
 
 export default class Review extends Component {
   constructor(props) {
@@ -16,15 +18,15 @@ export default class Review extends Component {
       return(
         <div className="reviews-grid-container">
           <div className="reviews-container review_user">
-            <div className="bold1"><span className="bold1">{review.username}</span></div>
-            <div className="bold1"><span className="bold1">Location:</span><span>{review.location}</span></div>
-            <div className="bold1"><span className="bold1">Age:</span><span>{review.age}</span></div>
+            <div>{review.username}</div>
+            <div>Location: <span className="light">{review.location}</span></div>
+            <div>Age: <span className="light">{review.age}</span></div>
           </div>
           <div className="reviews-container review_review">
-            <div><span>{review.title}</span></div>
-            <div><span>{review.review}</span></div>
-            <div><span>Fits:</span><span>{review.fits}</span></div>
-            <div><span>Size Purchased:</span><span>{review.size_purchased}</span></div>
+            <div><span className="reviewTitle">{review.title}</span></div>
+            <div><span className="light">{review.review}</span></div>
+            <div><span className="bold">Fits: </span><span className="light">{review.fits}</span></div>
+            <div><span className="bold">Size Purchased: </span><span className="light">{review.size_purchased}</span></div>
             <div><span>{review.recommends}</span></div>
           </div>
         </div>
@@ -40,6 +42,7 @@ export default class Review extends Component {
             <div>Height: <span className="light">{review.height}</span></div>
           </div>
           <div className="reviews-container review_review">
+            <div><StarRating rating={review.rating}/></div>
             <div><span className="reviewTitle">{review.title}</span></div>
             <div><span className="light">{review.review}</span></div>
             <div><span className="bold">Fits: </span><span className="light">{review.fits}</span></div>
---
+++ client/src/components/StarRating.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+const StarRating = (props) => {
+  switch(props.rating){
+    case 1:
+      return(
+        <React.Fragment>
+          <div className='starGroup'>
+            <div className='stars goldstars oneStar'></div>
+            <div className='stars graystars fourStar'></div>
+          </div>
+        </React.Fragment>          
+      );
+    case 2:
+      return(
+        <React.Fragment>
+            <div className='starGroup'>
+            <div className='stars goldstars twoStar'></div>
+            <div className='stars graystars threeStar'></div>
+            </div>
+        </React.Fragment>          
+        );
+    case 3:
+      return(
+        <React.Fragment>
+            <div className='starGroup'>
+            <div className='stars goldstars threeStar'></div>
+            <div className='stars graystars twoStar'></div>
+            </div>
+        </React.Fragment>          
+        );
+    case 4:
+      return(
+        <React.Fragment>
+            <div className='starGroup'>
+            <div className='stars goldstars fourStar'></div>
+            <div className='stars graystars oneStar'></div>
+            </div>
+          </React.Fragment>          
+        );
+    case 5:
+      return(
+        <div className='stars goldstars fiveStar'></div>
+      );
+    default:
+      return(
+        <div>
+          {`'' ${JSON.stringify(props)}`}
+        </div>  
+      ); 
+  }
+}
+
+export default StarRating;
