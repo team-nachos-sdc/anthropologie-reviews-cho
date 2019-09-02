96981ff6ff87f44889d94bf986666304e7973151
Ji Kim <jkim0107@gmail.com>
2019-09-02 11:45:20 -0700

    fixed spacing between colon and value

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -34,18 +34,18 @@ export default class Review extends Component {
         <div className="reviews-grid-container">
           <div className="reviews-container review_user">
             <div className="bold1"><span className="bold1">{review.username}</span></div>
-            <div className="bold1"><span className="bold1">Location:</span><span>{review.location}</span></div>
-            <div className="bold1"><span className="bold1">Age:</span><span>{review.age}</span></div>
-            <div className="bold1"><span className="bold1">Body Type:</span><span>{review.body_type}</span></div>
-            <div className="bold1"><span className="bold1">Height:</span><span>{review.height}</span></div>
+            <div>Location: <span>{review.location}</span></div>
+            <div className="bold1"><span className="bold1">Age: </span><span>{review.age}</span></div>
+            <div className="bold1"><span className="bold1">Body Type: </span><span>{review.body_type}</span></div>
+            <div className="bold1"><span className="bold1">Height: </span><span>{review.height}</span></div>
           </div>
           <div className="reviews-container review_review">
             <div><span>{review.title}</span></div>
             <div><span>{review.review}</span></div>
-            <div><span>Fits:</span><span>{review.fits}</span></div>
-            <div><span>Fit Purchased:</span><span>{review.fit_purchased}</span></div>
-            <div><span>Size Purchased:</span><span>{review.size_purchased}</span></div>
-            <div><span>Size Normally Worn:</span><span>{review.size}</span></div>
+            <div><span>Fits: </span><span>{review.fits}</span></div>
+            <div><span>Fit Purchased: </span><span>{review.fit_purchased}</span></div>
+            <div><span>Size Purchased: </span><span>{review.size_purchased}</span></div>
+            <div><span>Size Normally Worn: </span><span>{review.size}</span></div>
             <div><span>{review.recommends}</span></div>
           </div>
         </div>
