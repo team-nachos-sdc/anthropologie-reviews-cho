d4b224a7ff6c51cdce9345c4fa48bbd7618730e4
Hack Reactor Student <student@hackreactor.com>
2019-09-23 14:37:30 -0700

    added pool to the sql database to increase performance

--- database/indexSql.js
+++ database/indexSql.js
@@ -2,13 +2,18 @@ const Sequelize = require('sequelize');
 
 const db = new Sequelize ('anthropologie', 'pauld', 'password', {
   host: 'localhost',
-  dialect: 'postgres'
+  dialect: 'postgres',
+  pool: {
+    max: 1000,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
 })
 
 db
   .authenticate()
   .then(() => console.log('database connected'))
   .catch(err => console.log(`err connecting database ${err}`))
-
-
+  
 module.exports = db;
