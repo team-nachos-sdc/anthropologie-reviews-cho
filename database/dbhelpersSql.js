c292bdac7f90e0166d1040c540d7d6a53c71d0be
Hack Reactor Student <student@hackreactor.com>
2019-09-23 14:38:39 -0700

    working get request

--- database/dbhelpersSql.js
+++ database/dbhelpersSql.js
@@ -1,16 +1,21 @@
-const ProductModel = require('./indexSql.js');
-const { Products, Reviews } = require('./indexSql.js');
+const { Products, Reviews } = require('./modelsSql.js');
+const db = require('./indexSql');
+// const Sequelize = require('sequelize');
+
+// const getReview = (productId) => {
+//   return Products.findOne({product_id: productId})
+// } 
 
 const getReview = (productId) => {
-  return ProductModel.findOne({product_id: productId})
-} 
+   return db.query(`SELECT * FROM products RIGHT JOIN reviews ON products.category = reviews.category_id WHERE products.id = ${productId}`)
+}
+
 
 const postReview = (product_id, product_category, reviews) => {
   return ProductModel.create({product_id, product_category, reviews})
 }
 
-
-
+// 
 module.exports = {
   getReview,
   postReview
