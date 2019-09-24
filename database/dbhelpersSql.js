e2c2c809f01ca4c007086414ef0bd9fa2ea96db5
Hack Reactor Student <student@hackreactor.com>
2019-09-23 20:21:03 -0700

    fixed the bug

--- database/dbhelpersSql.js
+++ database/dbhelpersSql.js
@@ -1,22 +1,22 @@
-const { Products, Reviews } = require('./modelsSql.js');
-const db = require('./indexSql');
-// const Sequelize = require('sequelize');
+// const { Products, Reviews } = require('./modelsSql.js');
+// const db = require('./indexSql');
+// // const Sequelize = require('sequelize');
+
+// // const getReview = (productId) => {
+// //   return Products.findOne({product_id: productId})
+// // } 
 
 // const getReview = (productId) => {
-//   return Products.findOne({product_id: productId})
-// } 
-
-const getReview = (productId) => {
-   return db.query(`SELECT * FROM products RIGHT JOIN reviews ON products.category = reviews.category_id WHERE products.id = ${productId}`)
-}
+//    return db.query(`SELECT * FROM products RIGHT JOIN reviews ON products.category = reviews.category_id WHERE products.id = ${productId}`)
+// }
 
 
-const postReview = (product_id, product_category, reviews) => {
-  return ProductModel.create({product_id, product_category, reviews})
-}
+// const postReview = (product_id, product_category, reviews) => {
+//   return ProductModel.create({product_id, product_category, reviews})
+// }
 
-// 
-module.exports = {
-  getReview,
-  postReview
-}
+// // 
+// module.exports = {
+//   getReview,
+//   postReview
+// }
